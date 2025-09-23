1d084529924bfefe9e48772ee603788299bb04f6
Nurah Kamal <kmlnur001@myuct.ac.za>
2025-09-24 00:25:28 +0200

    added guest table for DB

---
+++ Database/Guest Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Guests (
+    GuestID INT PRIMARY KEY IDENTITY(1,1),
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100),
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    PostalCode VARCHAR(10),
+    DateCreated DATETIME DEFAULT GETDATE()
+);
