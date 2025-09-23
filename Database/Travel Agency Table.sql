065cfc88c47d1d57ede7ee8967932c656ecba0e9
Nurah Kamal <kmlnur001@myuct.ac.za>
2025-09-24 00:29:48 +0200

    added trvael agency table into DB

---
+++ Database/Travel Agency Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE TravelAgencies (
+    AgencyID INT PRIMARY KEY IDENTITY(1,1),
+    AgencyName VARCHAR(100) NOT NULL,
+    ContactPerson VARCHAR(100),
+    PhoneNumber VARCHAR(20),
+    Email VARCHAR(100)
+);
