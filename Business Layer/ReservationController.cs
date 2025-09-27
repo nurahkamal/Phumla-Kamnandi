f313981212519a9ec95af4cb2fd9f0f933fc271c
camryn-pillay <camrynjeanpillay@gmail.com>
2025-09-27 16:17:35 +0200

    1. Made changes to mutliple tables(SQL script also commited) 2. Added Classes(ReservationController, ReservationDB, RoomDB, RoomController) 3. Completed make a booking form backend

---
+++ Business Layer/ReservationController.cs
@@ -0,0 +1,49 @@
+﻿using Phumla_Kamnandi.Data_Layer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi.Business_Layer
+{
+    internal class ReservationController
+    {
+        private ReservationDB reservationDB = new ReservationDB();
+        private RoomDB roomDB = new RoomDB();
+
+        // Get room rate based on check-in date
+        public static decimal GetRoomRate(DateTime checkInDate)
+        {
+            if (checkInDate.Day >= 1 && checkInDate.Day <= 7)      // Low Season
+                return 550;
+            else if (checkInDate.Day >= 8 && checkInDate.Day <= 15) // Mid Season
+                return 750;
+            else                                                     // High Season
+                return 995;
+        }
+
+        // Check availability for requested number of rooms and dates
+        public bool IsFullyBooked(DateTime checkIn, DateTime checkOut, int requestedRooms)
+        {
+            return roomDB.IsFullyBooked(checkIn, checkOut, requestedRooms);
+        }
+
+        // Add reservation with automatic room allocation
+        public void CreateReservation(int guestID, int numberOfGuests, DateTime checkIn, DateTime checkOut)
+        {
+            int numberOfRooms = (int)Math.Ceiling(numberOfGuests / 4.0);
+            decimal roomRate = GetRoomRate(checkIn);
+
+            // Get list of available RoomIDs
+            List<int> availableRooms = roomDB.GetAvailableRooms(checkIn, checkOut, numberOfRooms);
+            if (availableRooms.Count < numberOfRooms)
+                throw new Exception("Not enough rooms available.");
+
+            // Insert into DB
+            reservationDB.InsertReservationWithRoomsAndAllocation(guestID, numberOfGuests, checkIn, checkOut, availableRooms, roomRate);
+        }
+    }
+
+}
