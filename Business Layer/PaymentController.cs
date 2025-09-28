ecb5d8800a44ea665709e7a58884be909e1ca491
camryn-pillay <camrynjeanpillay@gmail.com>
2025-09-28 12:57:48 +0200

    Added Payment, PaymentDB and PaymentController Classes

---
+++ Business Layer/PaymentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phumla_Kamnandi.Data_Layer;
+
+namespace Phumla_Kamnandi.Business_Layer
+{
+    internal class PaymentController
+    {
+        private PaymentDB paymentDB = new PaymentDB();
+
+        // Calculate total based on number of rooms and days
+        public decimal CalculateTotal(decimal roomPrice, int numberOfRooms, int numberOfDays)
+        {
+            return roomPrice * numberOfRooms * numberOfDays;
+        }
+
+        // Calculate 10% deposit
+        public decimal CalculateDeposit(decimal total)
+        {
+            return total * 0.10m;
+        }
+
+        // Display payment details in RichTextBox
+        public void DisplayPaymentDetails(System.Windows.Forms.RichTextBox richTextBox, Payment payment)
+        {
+            richTextBox.Clear();
+            richTextBox.AppendText($"Reservation ID: {payment.ReservationID}\n");
+            richTextBox.AppendText($"Account ID: {payment.AccountID}\n");
+            richTextBox.AppendText($"Payment Date: {payment.PaymentDate:d}\n");
+            richTextBox.AppendText($"Payment Type: {payment.PaymentType}\n");
+            richTextBox.AppendText($"Total Amount: {payment.TotalAmount:C}\n");
+            richTextBox.AppendText($"Deposit (10%): {payment.Deposit:C}\n");
+            richTextBox.AppendText($"Amount Paid: {payment.AmountPaid:C}\n");
+        }
+
+        // Save payment
+        public void SavePayment(Payment payment)
+        {
+            paymentDB.AddPayment(payment);
+        }
+    }
+}
---
+++ Data Layer/PaymentDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phumla_Kamnandi.Business_Layer;
+
+namespace Phumla_Kamnandi.Data_Layer
+{
+    internal class PaymentDB
+    {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=PhumlaKamnandiHotelsDB;Integrated Security=True";
+
+        public void AddPayment(Payment payment)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Payments (AccountID, ReservationID, PaymentDate, PaymentType, AmountPaid) " +
+                             "VALUES (@AccountID, @ReservationID, @PaymentDate, @PaymentType, @AmountPaid)";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@AccountID", payment.AccountID);
+                    cmd.Parameters.AddWithValue("@ReservationID", payment.ReservationID);
+                    cmd.Parameters.AddWithValue("@PaymentDate", payment.PaymentDate);
+                    cmd.Parameters.AddWithValue("@PaymentType", payment.PaymentType);
+                    cmd.Parameters.AddWithValue("@AmountPaid", payment.AmountPaid);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
