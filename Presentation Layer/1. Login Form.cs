ff21732f2df6e20830a867fa10231a97e0bd54be
Vanessa Mokoena <MKNREL009@myuct.ac.za>
2025-10-08 21:54:27 +0200

    Brought back Login code

--- Presentation Layer/1. Login Form.cs
+++ Presentation Layer/1. Login Form.cs
@@ -77,7 +77,17 @@ namespace Phumla_Kamnandi.Presentation_Layer
                 return;
             }
 
-           
+            /// password is succesful takes you to the about page
+            if (username.Equals("admin", StringComparison.OrdinalIgnoreCase) && password == "admin")
+            {
+                this.Hide();
+                About_Page aboutPage = new About_Page();
+                aboutPage.Show();
+            }
+            else
+            {
+                MessageBox.Show($"Invalid username or password. Attempt {loginAttempts} of {MaxLoginAttempts}.");
+            }
         }
 
 
