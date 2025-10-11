17dd4ed1f8bbf905b6d6b281b4d97e5eff7f254e
camryn-pillay <camrynjeanpillay@gmail.com>
2025-10-12 00:30:42 +0200

    Commit check

--- Presentation Layer/Guest Details.Designer.cs
+++ Presentation Layer/Guest Details.Designer.cs
@@ -337,7 +337,7 @@
             this.btnSignOut.Size = new System.Drawing.Size(117, 44);
             this.btnSignOut.TabIndex = 38;
             this.btnSignOut.Text = "Sign Out";
-            this.btnSignOut.Click += new System.EventHandler(this.btnSignOut_Click);
+            
             // 
             // btnReportIssue
             // 
@@ -354,8 +354,7 @@
             this.btnReportIssue.Size = new System.Drawing.Size(117, 44);
             this.btnReportIssue.TabIndex = 37;
             this.btnReportIssue.Text = "Report Issue";
-            this.btnReportIssue.Click += new System.EventHandler(this.btnReportIssue_Click);
-            // 
+            
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
@@ -651,7 +650,7 @@
             this.btnUser.Size = new System.Drawing.Size(272, 47);
             this.btnUser.TabIndex = 36;
             this.btnUser.Text = "User Information";
-            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            
             // 
             // btnAbout
             // 
@@ -671,7 +670,7 @@
             this.btnAbout.Size = new System.Drawing.Size(264, 43);
             this.btnAbout.TabIndex = 23;
             this.btnAbout.Text = "About Us";
-            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            
             // 
             // btnReports
             // 
@@ -691,7 +690,7 @@
             this.btnReports.Size = new System.Drawing.Size(260, 43);
             this.btnReports.TabIndex = 27;
             this.btnReports.Text = "  View Reports";
-            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            
             // 
             // btnUpdate
             // 
@@ -711,7 +710,7 @@
             this.btnUpdate.Size = new System.Drawing.Size(260, 43);
             this.btnUpdate.TabIndex = 25;
             this.btnUpdate.Text = "       Update booking";
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+           
             // 
             // btnHome
             // 
@@ -731,7 +730,7 @@
             this.btnHome.Size = new System.Drawing.Size(264, 43);
             this.btnHome.TabIndex = 35;
             this.btnHome.Text = "Home";
-            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+           
             // 
             // btnEnquiries
             // 
@@ -750,8 +749,8 @@
             this.btnEnquiries.Name = "btnEnquiries";
             this.btnEnquiries.Size = new System.Drawing.Size(260, 43);
             this.btnEnquiries.TabIndex = 26;
-            this.btnEnquiries.Text = "    Guest Enquries";
-            this.btnEnquiries.Click += new System.EventHandler(this.btnEnquiries_Click);
+           
+         
             // 
             // btnBooking
             // 
@@ -771,7 +770,7 @@
             this.btnBooking.Size = new System.Drawing.Size(264, 43);
             this.btnBooking.TabIndex = 24;
             this.btnBooking.Text = "      Make a Booking";
-            this.btnBooking.Click += new System.EventHandler(this.btnBooking_Click);
+            
             // 
             // frmCreateGuest
             // 
