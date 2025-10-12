cae1d706e003a90ff3c13fc08db4c4e63da2e6ee
Nurah Kamal <kmlnur001@myuct.ac.za>
2025-10-12 13:24:20 +0200

    deleted from

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace Phumla_Kamnandi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCreateGuest()); 
+            Application.Run(new User()); 
         }
     }
 }
